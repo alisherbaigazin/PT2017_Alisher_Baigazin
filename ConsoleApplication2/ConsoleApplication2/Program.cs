1210b66cbb057cfe11eeef6a0a82e756373b4646
Alisher Baigazin <alisherbaigazin@gmail.com>
2017-03-13 01:23:58 +0600

    midterm

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        class Polygon
+        {
+         
+        }
+        public static void getPerimeter(int a,int b)
+        {
+           int p = 2 * (a + b);
+        }
+        static void Main(string[] args)
+        {
+            DirectoryInfo info = new DirectoryInfo("C:\Users\1\Desktop\polygon");
+            DirectoryInfo[] dirs = info.GetDirectories();
+            FileInfo[] files = info.GetFiles();        
+            int d = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            getPerimeter(d, c);
+            Console.WriteLine(p);
+            Console.ReadKey();
+        
+        }
+    }
+}
--- ConsoleApplication2/ConsoleApplication2/Properties/AssemblyInfo.cs
+++ ConsoleApplication2/ConsoleApplication2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Управление общими сведениями о сборке осуществляется с помощью 
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("ConsoleApplication2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("ConsoleApplication2")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("a80e4860-6a74-4cd0-8972-921d44a70795")]
+[assembly: Guid("6031fc14-4ea0-4fb6-9dd2-73d251cf91e0")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
